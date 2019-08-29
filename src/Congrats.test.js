a4c02aacd9b286d6aa2774e4d7f716b1a0fd8199
Quentin <qgerard88@gmail.com>
2019-08-29 18:36:39 +0200

    Start tests for GuessedWords

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -18,7 +18,7 @@ test("renders without error", () => {
 });
 
 test("renders no text when `success` prop is false", () => {
-  const wrapper = setup({ success: false });
+  const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.text()).toBe("");
 });
