a0bc8011e3d9ac1e065bc070f6099ba50c436fbb
Quentin <qgerard88@gmail.com>
2019-08-29 22:08:47 +0200

    Setip Input component and storeFactory test util

---
+++ src/Input.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Input extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <button />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);
