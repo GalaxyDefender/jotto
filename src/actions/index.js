9093c2fbca8ceb218b260279f20fa67ad22627d5
Quentin <qgerard88@gmail.com>
2019-08-31 18:13:08 +0200

    Added Thunk for 'guessword'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,22 @@
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD"
 };
 
-export const guessWord = guessWord => (dispatch, getState) => {};
+export const guessWord = guessedWord => (dispatch, getState) => {
+  const secretWord = getState().secretWord;
+  const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+  dispatch({
+    type: actionTypes.GUESS_WORD,
+    payLoad: { guessedWord, letterMatchCount }
+  });
+
+  if (guessedWord === secretWord) {
+    dispatch({
+      type: actionTypes.CORRECT_GUESS
+    });
+  }
+};
